14fd94a02696a370897233093e798302656acf21
vijaydeyloyaltyone <vdey@loyalty.com>
2019-07-02 21:12:40 -0400

    Hacker rank problems initial checkin

--- hackerrank/src/main/java/com/vijay/study/easy/problemsolving/BiggerIsBetter.java
+++ hackerrank/src/main/java/com/vijay/study/easy/problemsolving/BiggerIsBetter.java
@@ -7,55 +7,64 @@ import java.util.List;
 //https://www.hackerrank.com/challenges/bigger-is-greater/problem
 public class BiggerIsBetter {
     public static String biggerIsGreater(String input) {
-        final char[] chars = input.toCharArray();
-        int i = input.length() - 2;
-        int maxIdx = input.length() - 1;
-        while(i >= 0) {
-            if(chars[maxIdx] > chars[i]) {
-                char tmp = chars[i];
-                chars[i] = chars[maxIdx];
-                chars[maxIdx] = tmp;
-
-                StringBuilder sb = new StringBuilder();
-                for(int j = 0 ; j < i; j++) {
-                    sb.append(chars[j]);
-                }
-
-                sb.append(chars[i]);
-
-                final List<Character> endingChars = new ArrayList<>();
-                for(int j = i + 1; j < input.length(); j++) {
-                    endingChars.add(chars[j]);
-                }
-
-                Collections.sort(endingChars);
-                for (Character c : endingChars)
-                    sb.append(c);
-                return sb.toString();
-            } else {
-                maxIdx = i;
-            }
-            i--;
-        }
-
-//        int i = input.length() - 1;
-//        char last = input.charAt(input.length() - 1);
-//        while(i > 0) {
-//            if(last > input.charAt(i - 1)) {
+//        final char[] chars = input.toCharArray();
+//        int i = input.length() - 2;
+//        int maxIdx = input.length() - 1;
+//        while(i >= 0) {
+//            if(chars[maxIdx] > chars[i]) {
+//                char tmp = chars[i];
+//                chars[i] = chars[maxIdx];
+//                chars[maxIdx] = tmp;
+//
+//                StringBuilder sb = new StringBuilder();
+//                for(int j = 0 ; j < i; j++) {
+//                    sb.append(chars[j]);
+//                }
+//
+//                sb.append(chars[i]);
 //
 //                final List<Character> endingChars = new ArrayList<>();
-//                for(int j = i - 1; j < input.length() - 1; j++) {
-//                    endingChars.add(input.charAt(j));
+//                for(int j = i + 1; j < input.length(); j++) {
+//                    endingChars.add(chars[j]);
 //                }
-//                Collections.sort(endingChars, (c1, c2) -> c1 - c2);
-//                StringBuilder sb = new StringBuilder();
-//                sb.append(last);
+//
+//                Collections.sort(endingChars);
 //                for (Character c : endingChars)
 //                    sb.append(c);
-//             return input.substring(0, i - 1)   + sb.toString();
+//                return sb.toString();
+//            } else {
+//                maxIdx = i;
 //            }
 //            i--;
 //        }
+
+        int j = input.length() - 1;
+        while(j > 0) {
+            int i = j;
+            char last = input.charAt(i);
+            while(i >= 0) {
+                if(last > input.charAt(i)) {
+                    StringBuilder sb = new StringBuilder();
+                    for(int k = 0; k < i; k++) {
+                        sb.append(input.charAt(k));
+                    }
+
+                    sb.append(input.charAt(j));
+
+                    final List<Character> endingChars = new ArrayList<>();
+                    for(int k = i; k < input.length(); k++) {
+                        if(k != j)
+                            endingChars.add(input.charAt(k));
+                    }
+                    Collections.sort(endingChars);
+                    for (Character c : endingChars)
+                        sb.append(c);
+                    return sb.toString();
+                }
+                i--;
+            }
+            j--;
+        }
         return "no answer";
 
     }
